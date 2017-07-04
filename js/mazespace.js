d671a77bd05f8527fe1a89f0aec8743c702a4429
jasminejeane <jasminejeane@gmail.com>
2017-07-04 16:43:02 -0700

    interface

--- js/mazespace.js
+++ js/mazespace.js
@@ -1,9 +1,12 @@
-'use strict';
+"use strict";
 
-function MazeSpace(){
-  this.north = false;
-  this.east = false;
-  this.south = false;
-  this.west = false;
+function MazeSpace(directions) {
+    var i;
+    for (i=0; i < directions.length; i += 1) {
+        this[directions[i]] = false;
+    }
+}
 
+MazeSpace.prototype.setWall = function(direction) {
+    this[direction] = true;
 }
